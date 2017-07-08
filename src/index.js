90d5a1537f7b9a86fda4afb07228e21e86e62dbb
Duknam Yoo <yoo2001818@gmail.com>
2017-07-08 23:47:41 +0900

    Add function prototypes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 import MemoryBackend from './backend/memory';
 
 export default class Index {
-  constructor(backend = new MemoryBackend()) {
+  constructor(backend = new MemoryBackend(), config = {}) {
+    // TODO Set the size to bigger value - this is only for testing.
+    const { key = 'id', size = 2 } = config;
     this.backend = backend;
+    this.config = { key, size };
     // TODO Load manifest information
     // TODO Manage each indexes
     // TODO Query builder
     // TODO Data manager
     // TODO Syncing
   }
-  loadManifest() {
-    
+  // Index manangement functions
+  addIndexes(indexes) {
+
+  }
+  addIndex(index) {
+
+  }
+  removeIndex(index) {
+
+  }
+  // Data management functions
+  add(document) {
+
+  }
+  remove(document) {
+
+  }
+  get(pk) {
+
+  }
+  commit() {
+
+  }
+  // Queries
+  explain(query) {
+
+  }
+  search(query) {
+
   }
 }
