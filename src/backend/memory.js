ee33fac3ef5b71cd1be30cb18a48af74ce48e16c
Duknam Yoo <yoo2001818@gmail.com>
2017-07-08 23:56:46 +0900

    Add memory backend prototype

---
+++ src/backend/memory.js
@@ -0,0 +1,27 @@
+export default class MemoryBackend {
+  constructor() {
+    // Nothing to do here yet
+  }
+  getManifest() {
+
+  }
+  setManifest(data) {
+
+  }
+  // Name 'null' is reversed to primary data storage, which stores actual data
+  // instead of bunch of PKs
+  // ID 0 is reversed to root
+  // TODO Wouldn't it be better to create specialized object for specific
+  // index? Although since they need disk access, it won't matter at all
+  // I think.
+  getIndexEntry(name, id) {
+    
+  }
+  setIndexEntry(name, id, data) {
+    // Data 'undefined' deletes the entry
+  }
+  // Commit all cache to disk (Which does nothing in memory backend)
+  commit() {
+    
+  }
+}
