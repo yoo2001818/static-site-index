a647cc0f700a31e6b5237a545f6e49913ff965d8
Duknam Yoo <yoo2001818@gmail.com>
2017-08-08 22:32:00 +0800

    Separate lt/gt bit flags

--- src/query/operator.js
+++ src/query/operator.js
@@ -1,4 +1,4 @@
-import { compare } from '../util/comparator';
+import { compare as compareValues } from '../util/comparator';
 
 // Operators are used to plan the query; User written JSONs are converted to
 // JSON with these operators.
@@ -50,9 +50,39 @@ const PAIR_BIT_FLAGS = {
   eq: 1,
   ne: 2,
   lt: 4,
-  gt: 4,
-  range: 8,
+  gt: 8,
+  range: 16,
 };
+
+// Used to mark the lt/gt as a range query.
+const MAX_INFINITY = Symbol('Infinity');
+const MIN_INFINITY = Symbol('-Infinity');
+
+function convertToRange(v) {
+  if (v.type === 'gt') {
+    return {
+      type: 'range',
+      gt: v.value,
+      gte: v.equal,
+      lt: MAX_INFINITY,
+      lte: false,
+    };
+  } else {
+    return {
+      type: 'range',
+      lt: v.value,
+      lte: v.equal,
+      gt: MIN_INFINITY,
+      gte: false,
+    };
+  }
+}
+
+function compare(a, b) {
+  if (a === MAX_INFINITY || b === MIN_INFINITY) return 1;
+  if (a === MIN_INFINITY || b === MAX_INFINITY) return -1;
+  return compareValues(a, b);
+}
 
 export function union(a, b) {
   // Case 1. eq/eq, ne/ne - Concatenate two values.
@@ -97,25 +127,45 @@ export function union(a, b) {
         type: 'ne',
         values: high.values.filter(v => !low.values.includes(v)),
       };
-    case 4: // (l|g)t & (l|g)t
+    case 4: { // lt & lt
+      // Pick the largest value. If both values are same, pick the one with
+      // equal flag.
+      let compared = compare(a.value, b.value);
+      let winner = compared > 0 ? true : (compared === 0 ? a.equal : false);
+      return winner ? a : b;
+    }
+    case 5: // lt & eq
+      break;
+    case 6: // lt & ne
 
       break;
-    case 5: // (l|g)t & eq
+    case 8: { // gt & gt
+      // Pick the smallest value. If both values are same, pick the one with
+      // equal flag.
+      let compared = compare(a.value, b.value);
+      let winner = compared < 0 ? true : (compared === 0 ? a.equal : false);
+      return winner ? a : b;
+    }
+    case 9: // gt & eq
 
       break;
-    case 6: // (l|g)t & ne
+    case 10: // gt & ne
 
       break;
-    case 8: // range & range
+    case 12: // gt & lt
 
       break;
-    case 9: // range & eq
+    case 16: // range & range
+      
+      break;
+    case 17: // range & eq
 
       break;
-    case 10: // range & ne
+    case 18: // range & ne
 
       break;
-    case 12: // range & (l|g)t
+    case 20: // range & lt
+    case 24: // range & gt
 
       break;
   }
