51ee099f91e9ddaad47ecf9932d818abd12df314
Duknam Yoo <yoo2001818@gmail.com>
2017-08-16 23:55:48 +0900

    Add basic operators

--- src/query/operator.js
+++ src/query/operator.js
@@ -12,9 +12,44 @@ import { compare as compareValues } from '../util/comparator';
 // Each operator has many instructions, telling that what range/value should be
 // accepted or not.
 //
-// There are these types of instructions: <, >, ==, !=.
+// There are these types of instructions: <, >, <=, >=, ==, !=.
 // It's pretty self explainary. < and > continues until they meet corresponding
 // > or <. != ignores the specific value in the range. You can see it as a very
 // specific < and >.
 //
 // [ > 1, < 3 ] means 1 < n < 3.
+// [ < 3 ] means -Infinity < n < 3.
+//
+// However, != requires that the range is already 'triggered'. For this
+// purpose, special instruction '*' is used to mark that all range is
+// qualified.
+
+export function gt(value, equal = false) {
+  return [
+    { type: '>', value, equal },
+  ];
+}
+
+export function lt(value, equal = false) {
+  return [
+    { type: '<', value, equal },
+  ];
+}
+
+export function range(gt, lt, gte = false, lte = false) {
+  return [
+    { type: '>', value: gt, equal: gte },
+    { type: '<', value: lt, equal: lte },
+  ];
+}
+
+// This assumes that the values array is sorted.
+// TODO Check if the array is sorted, and sort them if required
+export function eq(values) {
+  return values.map(v => ({ type: '=', value: v }));
+}
+
+export function neq(values) {
+  return [{ type: '*' }].concat(
+    values.map(v => ({ type: '=', value: v })));
+}
