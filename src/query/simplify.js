23a80c05ae2c333fbd3d0f95921f13c47bbee803
Duknam Yoo <yoo2001818@gmail.com>
2017-09-13 23:54:43 +0900

    Change simplify structure

--- src/query/simplify.js
+++ src/query/simplify.js
@@ -1,26 +1,47 @@
+import { gt, lt, range, eq, neq, not, or, and } from './operator';
+
 export default function simplifyQuery(query) {
-  // Traverse every property of the query and try to simplify it to use
-  // 'simple' operators.
-  // The output is a 3D array - Each axis is for AND, OR, AND.
-  // Each subset is expected to return 3D array too. It'll be merged then.
-  return processSubset(query, false);
+  // Traverse every property of the query and try to simplify it to check it
+  // against the indexes.
+  // It returns a retrieval range of each key. After that, the retrieval costs
+  // will be calculated for each keys.
+  return processSubset(query);
 }
 
-function processSubset(query, invert = false) {
-  let output = [];
+function objectEach(src, op) {
+  let output = {};
+  for (let key in src) {
+    output[key] = op(src[key], key);
+  }
+  return output;
+}
+
+function mergeSubset(dest, src, op) {
+  for (let key in src) {
+    dest[key] = op(dest[key], src[key], key);
+  }
+}
+
+function processSubset(query) {
+  let keys = {};
   for (let key in query) {
+    // TODO Handle arrays
     switch (key) {
       case '$and':
+        mergeSubset(keys, processSubset(query[key]), and);
         break;
       case '$not':
+        mergeSubset(keys, not(processSubset(query[key])), and);
         break;
       case '$or':
+        mergeSubset(keys, processSubset(query[key]), or);
         break;
       case '$nor':
+        // Uhh I don't get it
         break;
       default:
         break;
     }
   }
-  return output;
+  return keys;
 }
