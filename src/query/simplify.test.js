a4387ecb49dd9d83aa38e56aafb7c34f5dc4bad5
Duknam Yoo <yoo2001818@gmail.com>
2017-09-16 23:56:06 +0900

    Add or / and / object in object tests

--- src/query/simplify.test.js
+++ src/query/simplify.test.js
@@ -69,4 +69,84 @@ describe('simplify', () => {
       b: neq([3, 6, 9]),
     });
   });
+  it('should handle exists query', () => {
+    expect(simplify({
+      a: { exists: false }, b: { exists: true },
+    })).toEqual({
+      a: eq([undefined]),
+      b: neq([undefined]),
+    });
+  });
+  it('should handle exists or', () => {
+    expect(simplify({
+      a: 1,
+      b: 3,
+      $or: [
+        { c: 5, b: 3 },
+        { c: 7, a: 5 },
+      ],
+    })).toEqual({
+      // A becomes nothing since a: 1 and a: 5 both can't be satisfied - thus,
+      // this will return zero results.
+      a: [],
+      b: eq([3]),
+      c: eq([5, 7]),
+    });
+    expect(simplify({
+      a: {
+        $or: [1, 5, 9, { $gt: 15 }],
+      },
+    })).toEqual({
+      a: or(eq([1, 5, 9]), gt(15)),
+    });
+  });
+  it('should handle exists and', () => {
+    expect(simplify({
+      a: 1,
+      b: 3,
+      $and: [{
+        c: 3,
+      }],
+    })).toEqual({
+      a: eq([1]),
+      b: eq([3]),
+      c: eq([3]),
+    });
+    expect(simplify({
+      a: {
+        $and: [{ $gt: 15 }, { $lt: 99 }],
+      },
+    })).toEqual({
+      a: range(15, 99),
+    });
+  });
+  it('should handle object in object', () => {
+    expect(simplify({
+      pos: {
+        x: 1,
+        y: 2,
+      },
+    })).toEqual({
+      'pos.x': eq([1]),
+      'pos.y': eq([2]),
+    });
+    expect(simplify({
+      pos: {
+        $or: [{
+          x: 1,
+          y: 2,
+        }, {
+          dimension: { $gte: 3 },
+          x: 5,
+          y: 5,
+        }],
+      },
+    })).toEqual({
+      // This happens because the first one didn't specify the dimension -
+      // thus, it automatically becomes '*'.
+      'pos.dimension': neq([]),
+      'pos.x': eq([1, 5]),
+      'pos.y': eq([2, 5]),
+    });
+  });
 });
